69c82a337427b747dfb9e8986418a81aced05080
Dinh Le <lqdquangdinh@gmail.com>
2023-07-26 03:36:06 +0300

    add random card func

--- src/components/Play/Play.tsx
+++ src/components/Play/Play.tsx
@@ -4,25 +4,46 @@ import { useState, useEffect } from 'react';
 import PlayingCard from '../../models/playingCard';
 
 const Play = () => {
-  //   const [cardDeck, setCardDeck] = useState<PlayingCard[]>([]);
-  let deck: PlayingCard[];
+  const [cardDeck, setCardDeck] = useState<PlayingCard[]>([]);
+  const [currentCard, setCurrentCard] = useState<PlayingCard | null>(null);
 
   function buildCardDeck() {
     const suits: string[] = ['spades', 'clubs', 'diamonds', 'hearts'];
 
     //use modern js instead of nested for loop
-    deck = suits.flatMap((suit) =>
+    const deck = suits.flatMap((suit) =>
       Array.from({ length: 13 }, (_, index) => new PlayingCard(index + 2, suit))
     );
 
     console.log(deck);
+    setCardDeck(deck);
   }
 
-//   useEffect(() => {
-//     buildCardDeck();
-//   }, []);
+  function pickRandomCard() {
+    if (cardDeck.length === 0) {
+      console.log('No cards left in the deck!');
+      setCurrentCard(null);
+      return;
+    }
 
-  buildCardDeck();
+    const randomIndex = Math.floor(Math.random() * cardDeck.length);
+    const pickedCard = cardDeck[randomIndex];
+
+    // Create a new array without the picked card
+    const updatedDeck = cardDeck.filter((card) => card !== pickedCard);
+
+    setCurrentCard(pickedCard);
+    setCardDeck(updatedDeck);
+    console.log('Picked Card:', pickedCard);
+  }
+
+  useEffect(() => {
+    buildCardDeck();
+    pickRandomCard();
+  }, []);
+
+  //   use buildCardDeck() in useEffect to make it execute only once
+  // if the function is outside of useEffect, it will trigger every render
 
   return (
     <Container
