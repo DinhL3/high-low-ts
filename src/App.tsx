29461525384423bd0ee845d1e1f8ec691b049603
Dinh Le <lqdquangdinh@gmail.com>
2023-07-31 00:39:06 +0300

    add button toggle, color black to theme

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,41 @@ import {
   createTheme,
   responsiveFontSizes,
   ThemeProvider,
+  PaletteColorOptions,
 } from '@mui/material/styles';
 
 import Home from './components/Home/Home';
 import Play from './components/Play/Play';
 
+declare module '@mui/material/styles' {
+  interface CustomPalette {
+    black: PaletteColorOptions;
+    // add more colors here
+  }
+  interface Palette extends CustomPalette {}
+  interface PaletteOptions extends CustomPalette {}
+}
+
+declare module '@mui/material/Button' {
+  interface ButtonPropsColorOverrides {
+    black: true;
+    //add more colors here
+  }
+}
+
+const { palette } = createTheme();
+const { augmentColor } = palette;
+const createColor = (mainColor: string) =>
+  augmentColor({ color: { main: mainColor } });
+
 let theme = createTheme({
   typography: {
     fontFamily: ['Kanit', 'sans-serif'].join(','),
-  },  
+  },
+  palette: {
+    black: createColor('#000000'),
+    //add more colors here
+  },
 });
 
 theme = responsiveFontSizes(theme);
--- src/components/Play/BettingForm.tsx
+++ src/components/Play/BettingForm.tsx
@@ -1,58 +1,116 @@
-import { Button, Container, Typography, Box } from '@mui/material';
+// Component that handle betting form. Includes toggle buttons, bet amount input and submit button
+
+import { useState } from 'react';
+import {
+  Button,
+  Container,
+  Typography,
+  Box,
+  FormControl,
+  TextField,
+} from '@mui/material';
 import KeyboardDoubleArrowUpIcon from '@mui/icons-material/KeyboardDoubleArrowUp';
 import KeyboardDoubleArrowDownIcon from '@mui/icons-material/KeyboardDoubleArrowDown';
 import { CardsContext } from '../../store/cards-context';
 
 const BettingForm = () => {
+  const [selectedHigherOrLower, setSelectedHigherOrLower] = useState<
+    null | 'higher' | 'lower'
+  >(null);
+  const [selectedColor, setSelectedColor] = useState<null | 'black' | 'red'>(
+    null
+  );
+  const [selectedRange, setSelectedRange] = useState<null | '2-10' | 'JQKA'>(
+    null
+  );
+
+  function handleHigherOrLowerClick(selected: 'higher' | 'lower'): void {
+    setSelectedHigherOrLower((prevSelected) =>
+      prevSelected === selected ? null : selected
+    );
+  }
+
+  function handleColorClick(selected: 'black' | 'red'): void {
+    setSelectedColor((prevSelected) =>
+      prevSelected === selected ? null : selected
+    );
+  }
+
+  function handleRangeClick(selected: '2-10' | 'JQKA'): void {
+    setSelectedRange((prevSelected) =>
+      prevSelected === selected ? null : selected
+    );
+  }
+
   return (
-    <Box className='button-columns-box' sx={{ display: 'flex', gap: 2 }}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-        <Button
-          variant='outlined'
-          color='primary'
-          disableElevation
-          startIcon={<KeyboardDoubleArrowUpIcon />}
-        >
-          Higher or same
-        </Button>
-        <Button
-          sx={{}}
-          variant='outlined'
-          color='warning'
-          disableElevation
-          startIcon={<KeyboardDoubleArrowDownIcon />}
-        >
-          Lower or same
-        </Button>
+    <FormControl>
+      <Box className='button-columns-box' sx={{ display: 'flex', gap: 2 }}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+          <Button
+            variant={
+              selectedHigherOrLower === 'higher' ? 'contained' : 'outlined'
+            }
+            color='primary'
+            disableElevation
+            startIcon={<KeyboardDoubleArrowUpIcon />}
+            onClick={() => handleHigherOrLowerClick('higher')}
+          >
+            Higher or same
+          </Button>
+          <Button
+            sx={{}}
+            variant={
+              selectedHigherOrLower === 'lower' ? 'contained' : 'outlined'
+            }
+            color='warning'
+            disableElevation
+            startIcon={<KeyboardDoubleArrowDownIcon />}
+            onClick={() => handleHigherOrLowerClick('lower')}
+          >
+            Lower or same
+          </Button>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+          <Button
+            // sx={{ color: 'black' }}
+            variant={selectedColor === 'black' ? 'contained' : 'outlined'}
+            color='black'
+            disableElevation
+            startIcon={'♠♣'}
+            onClick={() => handleColorClick('black')}
+          >
+            Black
+          </Button>
+          <Button
+            variant={selectedColor === 'red' ? 'contained' : 'outlined'}
+            color='error'
+            disableElevation
+            startIcon={'♦♥'}
+            onClick={() => handleColorClick('red')}
+          >
+            Red
+          </Button>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+          <Button
+            variant={selectedRange === '2-10' ? 'contained' : 'outlined'}
+            color='primary'
+            disableElevation
+            onClick={() => handleRangeClick('2-10')}
+          >
+            2-10
+          </Button>
+          <Button
+            variant={selectedRange === 'JQKA' ? 'contained' : 'outlined'}
+            color='secondary'
+            disableElevation
+            onClick={() => handleRangeClick('JQKA')}
+          >
+            JQKA
+          </Button>
+        </Box>
       </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-        <Button
-          sx={{ color: 'black' }}
-          variant='outlined'
-          color='inherit'
-          disableElevation
-          startIcon={'♠♣'}
-        >
-          Black
-        </Button>
-        <Button
-          variant='outlined'
-          color='error'
-          disableElevation
-          startIcon={'♦♥'}
-        >
-          Red
-        </Button>
-      </Box>
-      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
-        <Button variant='outlined' color='primary' disableElevation>
-          2-10
-        </Button>
-        <Button variant='outlined' color='secondary' disableElevation>
-          JQKA
-        </Button>
-      </Box>
-    </Box>
+    </FormControl>
   );
 };
 
