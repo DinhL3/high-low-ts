eb8f8601cfe029149a6fb43ba4a7a7efddf1a5a4
Dinh Le <lqdquangdinh@gmail.com>
2023-08-01 00:56:49 +0300

    add betAmount, error checks

--- src/components/Play/BettingForm.tsx
+++ src/components/Play/BettingForm.tsx
@@ -1,10 +1,9 @@
 // Component that handle betting form. Includes toggle buttons, bet amount input and submit button
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Button,
-  Container,
-  Typography,
+  Alert,
   Box,
   FormControl,
   TextField,
@@ -23,28 +22,82 @@ const BettingForm = () => {
   const [selectedRange, setSelectedRange] = useState<null | '2-10' | 'JQKA'>(
     null
   );
+  const [betAmount, setBetAmount] = useState<string>('');
+  const [betAmountError, setBetAmountError] = useState<string | null>(null);
+  const [isNoGuessSelected, setIsNoGuessSelected] = useState<boolean>(true);
 
   function handleHigherOrLowerClick(selected: 'higher' | 'lower'): void {
     setSelectedHigherOrLower((prevSelected) =>
       prevSelected === selected ? null : selected
-    );
+    )    
   }
 
   function handleColorClick(selected: 'black' | 'red'): void {
     setSelectedColor((prevSelected) =>
       prevSelected === selected ? null : selected
-    );
+    );    
   }
 
   function handleRangeClick(selected: '2-10' | 'JQKA'): void {
     setSelectedRange((prevSelected) =>
       prevSelected === selected ? null : selected
-    );
+    );    
   }
+
+  function handleBetAmountChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const betAmountInput = event.target.value;
+
+    const positiveIntegerPattern = /^[1-9]\d*$/;
+
+    if (!positiveIntegerPattern.test(betAmountInput)) {
+      setBetAmountError('Please enter a valid bet amount');
+    } else {
+      setBetAmountError(null);
+    }
+
+    setBetAmount(betAmountInput);
+  }
+
+  // put this function in useEffect is correct, because it will be called every time state of buttons change
+  // the function depends on state of buttons, not the clicks
+  function handleGuessSelectionCheck(): void {
+    if (!selectedHigherOrLower && !selectedColor && !selectedRange) {
+      setIsNoGuessSelected(true);
+    } else {
+      setIsNoGuessSelected(false);
+    }
+  }
+
+  function handleSubmit(): void {
+    if (isNoGuessSelected) {
+      return;
+    }
+
+    if (betAmountError || !betAmount) {
+      setBetAmountError('Please enter a valid bet amount');
+      return;
+    }
+
+    const betData = {
+      selectedHigherOrLower: selectedHigherOrLower,
+      selectedColor: selectedColor,
+      selectedRange: selectedRange,
+      betAmount: Number(betAmount),
+    };
+
+    console.log('Bet Data:', betData);
+  }
+
+  useEffect(() => {
+    handleGuessSelectionCheck();
+  }, [selectedColor, selectedHigherOrLower, selectedRange]);
 
   return (
     <FormControl>
-      <Box className='button-columns-box' sx={{ display: 'flex', gap: 2 }}>
+      <Box
+        className='button-columns-box'
+        sx={{ display: 'flex', gap: 2, mb: 2 }}
+      >
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
           <Button
             variant={
@@ -72,7 +125,6 @@ const BettingForm = () => {
         </Box>
         <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
           <Button
-            // sx={{ color: 'black' }}
             variant={selectedColor === 'black' ? 'contained' : 'outlined'}
             color='black'
             disableElevation
@@ -110,6 +162,31 @@ const BettingForm = () => {
           </Button>
         </Box>
       </Box>
+      {isNoGuessSelected && (
+        <Alert sx={{ mb: 2 }} severity='error'>
+          Please choose at least one guess above
+        </Alert>
+      )}
+      <TextField
+        id='bet-amount'
+        label='Bet amount'
+        sx={{ mb: 2 }}
+        InputLabelProps={{
+          shrink: true,
+        }}
+        onChange={handleBetAmountChange}
+        error={Boolean(betAmountError)}
+        helperText={betAmountError}
+      />
+      <Button
+        type='submit'
+        variant='contained'
+        color='success'
+        disableElevation
+        onClick={handleSubmit}
+      >
+        Bet
+      </Button>
     </FormControl>
   );
 };
